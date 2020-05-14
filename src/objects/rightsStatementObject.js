b80ec479d5496f0a08733a575ef6bd06cade50b6
Tarje Lavik <tarjelavik@tarjes-imac.lan>
2020-05-14 20:30:12 +0200

    WIP first stab at rights

---
+++ src/objects/rightsStatementObject.js
@@ -0,0 +1,40 @@
+import { GiHandOfGod } from "react-icons/gi";
+import { label } from "../props";
+import { defaultFieldsets } from "../fieldsets";
+
+export default {
+  title: "Right statement",
+  name: "rightsStatementObject",
+  type: "object",
+  icon: GiHandOfGod,
+  fieldsets: defaultFieldsets,
+  fields: [
+    label, 
+    {
+      name: "inheritFrom",
+      title: "Arver fra",
+      titleEN: "Inherits from",
+      description: "Arver rettigheter fra predefinert lisens.",
+      type: "url"
+    },
+    {
+      name: "deprecatedOn",
+      title: "Gyldig til",
+      titleEN: "Deprecated on",
+      description: "Dato rettighetene utløper.",
+      descriptionEN: "Date the rights deprecates",
+      type: "date"
+    },
+  ],
+  preview: {
+    select: {
+      title: "label.nor",
+    },
+    prepare(selection) {
+      const { title } = selection;
+      return {
+        title: title.no,
+      };
+    },
+  },
+};
