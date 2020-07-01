8edfc2811b212ad994d1ba4c9f36b07ffb378c0f
Tarje Sælen Lavik <tarje.lavik@gmail.com>
2020-07-01 12:18:08 +0200

    Fix schema errors hidden when testing

---
+++ src/documents/visualItem.js
@@ -0,0 +1,39 @@
+import {FaImage} from 'react-icons/fa'
+
+export default {
+  title: 'Visual object',
+  name: 'visualItem',
+  type: 'document',
+  icon: FaImage,
+  fields: [
+    {
+      name: 'label',
+      title: 'Tittel',
+      titleEN: 'Title',
+      type: 'localeString',
+      validation: Rule => Rule.required()
+    },
+    {
+      name: 'activityStream',
+      title: 'Aktivitetsstrøm',
+      titleEN: 'Activity stream',
+      description: 'Events and activities connected to this object',
+      type: 'array',
+      of: [
+        {type: 'creation'}
+      ]
+    }
+  ],
+  preview: {
+    select: {
+      title: 'label.nor'
+    },
+    prepare (selection) {
+      const {title} = selection
+
+      return {
+        title: title
+      }
+    }
+  }
+}
